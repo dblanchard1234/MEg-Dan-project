659a7794ff5926da0a4acd5623e682da2cbf79aa
PWD12 <PWD12@baypath.net>
2017-10-04 14:15:36 -0400

    created an obstacle rectangle and obstacle class

--- MDP/MDP/Game1.cs
+++ MDP/MDP/Game1.cs
@@ -53,6 +53,7 @@ namespace MDP
 
 
             base.Draw(gameTime);
+
         }
     }
 }
--- MDP/MDP/Obstacle.cs
+++ MDP/MDP/Obstacle.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace MDP
 {
-    class obstacle
+    public class Obstacle : DrawableGameComponent
     {
+        private Texture2D solidTexture;
+        private Rectangle bBox;
+        private Color color;
+        private SpriteBatch sb;
+
+        public Obstacle(Game game, SpriteBatch sb, int x, int y, int xv, int yv) : base(game)
+        {
+
+            this.sb = sb;
+            solidTexture = new Texture2D(GraphicsDevice, 1, 1);
+            solidTexture.SetData(new Color[] { Color.White });
+            bBox = new Rectangle(x, y, 50, 50);
+            color = Color.Red;
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            sb.Draw(solidTexture, bBox, color);
+            base.Draw(gameTime);
+        }
     }
 }
